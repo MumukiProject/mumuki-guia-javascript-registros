0ebfcb15290563468ed672f6a25d1694a82eda83
franco@mumuki.org <franco@mumuki.org>
2020-08-03 13:44:58 +0000

    Mumuki Export on 2020-08-03 13:44:58 +0000

--- 00001_Los primeros registros/extra.js
+++ 00001_Los primeros registros/extra.js
@@ -1,5 +1,8 @@
+/*<hidden-for-student@*/
 let estatuaDeLaLibertad = { nombre: "Estatua de la Libertad", locacion: "Nueva York, Estados Unidos de América", anioDeConstruccion: 1886 };
 let cristoRedentor = { nombre: "Cristo Redentor", locacion: "Rio de Janeiro, Brasil", anioDeConstruccion: 1931 };
 let torreEiffel = { nombre: "Torre Eiffel", locacion: "París, Francia", anioDeConstruccion: 1889 };
 let tajMahal = { nombre: "Taj Mahal", locacion: "Agra, India", anioDeConstruccion: 1653 };
 let coliseo = { nombre: "Coliseo", locacion: "Roma, Italia", anioDeConstruccion: 80 };
+/*@hidden-for-student>*/
+
