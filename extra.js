01390f9f4175926693e69942783e75e6f009f2fa
gustavo@mumuki.org <gustavo@mumuki.org>
2021-11-29 16:34:22 +0000

    Mumuki Export on 2021-11-29 16:34:22 +0000

--- extra.js
+++ extra.js
@@ -6,17 +6,6 @@
 // =============
 /*@hidden-for-student>*/
 
-function longitud(unString) /*<elipsis-for-student@*/ {
-  return unString.length;
-} /*@elipsis-for-student>*/
-// Retorna cuan largo es un string
-//
-// Por ejemplo:
-//
-//  ム longitud("hola")
-//  4
-
-
 function convertirEnMayuscula(unString) /*<elipsis-for-student@*/ {
   return unString.toUpperCase()
 } /*@elipsis-for-student>*/
